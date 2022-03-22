bce7759f5ff0010dd8dbe16c170554a7293b063d
Sahara <THsahara@gmail.com>
2022-03-22 14:24:55 -0400

    started Adopt.js

---
+++ src/Adopt.js
@@ -0,0 +1,40 @@
+
+
+function Adopt() {
+    return(
+        <div>
+            <div>
+                <h2 className="text-2xl font-bold text-atomic-tangerine-500">Adoptable Animals</h2>
+            </div>
+            <div className="flex justify-evenly">
+                <div className="border-4 border-powder-blue-500 p-3">
+                    <div></div>
+                    <div className="flex space-x-3">
+                        <ul>age</ul>
+                        <ul>breed</ul>
+                        <ul>shelter</ul>
+                    </div>
+                </div>
+                <div className="border-4 border-powder-blue-500 p-3">
+                    <div></div>
+                    <div className="flex space-x-3">
+                        <ul>age</ul>
+                        <ul>breed</ul>
+                        <ul>shelter</ul>
+                    </div>
+                </div>
+                <div className="border-4 border-powder-blue-500 p-3">
+                    <div></div>
+                    <div className="flex space-x-3">
+                        <ul>age</ul>
+                        <ul>breed</ul>
+                        <ul>shelter</ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Adopt;
+
