0be73bee7a11c5372e57572c4e0cdc212d49020c
Sahara <THsahara@gmail.com>
2022-03-28 16:17:11 -0400

    Changed Adopt.js and added Animals.js

--- src/Adopt.js
+++ src/Adopt.js
@@ -6,32 +6,10 @@ function Adopt() {
             <div>
                 <h2 className="text-2xl font-bold text-atomic-tangerine-500">Adoptable Animals</h2>
             </div>
-            <div className="flex justify-evenly">
-                <div className="border-4 border-powder-blue-500 p-3 rounded-lg">
-                    <div>
-                    </div>
-                    <div className="flex space-x-3">
-                        <ul>age</ul>
-                        <ul>breed</ul>
-                        <ul>shelter</ul>
-                    </div>
-                </div>
-                <div className="border-4 border-powder-blue-500 p-3 rounded-lg">
-                    <div></div>
-                    <div className="flex space-x-3">
-                        <ul>age</ul>
-                        <ul>breed</ul>
-                        <ul>shelter</ul>
-                    </div>
-                </div>
-                <div className="border-4 border-powder-blue-500 p-3 rounded-lg">
-                    <div></div>
-                    <div className="flex space-x-3">
-                        <ul>age</ul>
-                        <ul>breed</ul>
-                        <ul>shelter</ul>
-                    </div>
-                </div>
+            <div>
+                <img src={image} alt={name} />
+                <h3>{name}</h3>
+                <p>{age}</p>
             </div>
         </div>
     );
