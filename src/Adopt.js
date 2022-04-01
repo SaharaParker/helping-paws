0735b7925cc06bda79589bf508cc4d878e453dd8
Sahara <THsahara@gmail.com>
2022-04-01 12:35:15 -0400

    added bg to Adopt.js

--- src/Adopt.js
+++ src/Adopt.js
@@ -3,7 +3,7 @@ function Adopt({pet_name, image, age, breed, location}) {
     return(
         <div className="text-center border-4 border-powder-blue-500">
             <div className="adoptable">
-               <img src={image} alt={pet_name} className="md:h-72 md:w-72 p-2" />
+               <img src={image} alt={pet_name} className="md:h-72 md:w-72 p-2 bg-sandwisp-50" />
                 <h1 className="font-bold text-3xl text-atomic-tangerine-500">{pet_name}</h1>
                 <p>{age} | {breed} | {location}</p>
                 <button className="bg-powder-blue-500 hover:bg-foam-500 border-4 border-biscay-500">Apply to Adopt</button>
