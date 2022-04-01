e3df29d5dab77e12648e6f84d568dd85b4f06356
Sahara <THsahara@gmail.com>
2022-04-01 11:55:54 -0400

    made all images same size

--- src/Adopt.js
+++ src/Adopt.js
@@ -3,7 +3,7 @@ function Adopt({pet_name, image, age, breed, location}) {
     return(
         <div className="border-4 border-powder-blue-500">
             <div className="adoptable">
-               <img src={image} alt={pet_name} />
+               <img src={image} alt={pet_name} className="h-72 w-72" />
                 <h1 className="font-bold text-3xl text-atomic-tangerine-500">{pet_name}</h1>
                 <p>{age} | {breed} | {location}</p>
                 <button className="bg-powder-blue-500 hover:bg-foam-500 border-4 border-biscay-500">Apply to Adopt</button>
