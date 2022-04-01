bef8f5f782e66aa7bab53ed03a7850a562452290
Sahara <THsahara@gmail.com>
2022-04-01 11:51:34 -0400

    added adoption button

--- src/Adopt.js
+++ src/Adopt.js
@@ -6,6 +6,7 @@ function Adopt({pet_name, image, age, breed, location}) {
                <img src={image} alt={pet_name} />
                 <h1 className="font-bold text-3xl text-atomic-tangerine-500">{pet_name}</h1>
                 <p>{age} | {breed} | {location}</p>
+                <button className="bg-powder-blue-500 hover:bg-foam-500 border-4 border-biscay-500">Apply to Adopt</button>
             </div>
         </div>
     );
